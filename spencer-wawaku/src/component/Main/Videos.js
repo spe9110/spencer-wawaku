ba5cae36bc0162be9e44fd16e9620fb3cb84fdda
Spencer WAWAKU <spencerm365@outlook.fr>
2024-07-13 12:34:15 +0200

    add this videos pages update

--- spencer-wawaku/src/component/Main/Videos.js
+++ spencer-wawaku/src/component/Main/Videos.js
@@ -43,11 +43,20 @@ export default function Videos(){
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 infinite: true,
                 dots: true
               }
