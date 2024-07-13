8579fd469c8fb8deac7a75fc707652f8e2c18cf6
Spencer WAWAKU <spencerm365@outlook.fr>
2024-07-14 00:44:22 +0200

    add the full contact page and whole the pages it is now full responsive

--- spencer-wawaku/src/component/Main/AlertMessage/Alert.js
+++ spencer-wawaku/src/component/Main/AlertMessage/Alert.js
@@ -3,10 +3,13 @@ import {
     AlertDialogBody, 
     AlertDialogContent, 
     AlertDialogHeader, 
-    AlertDialogOverlay, 
+    AlertDialogOverlay,
+    AlertDialogFooter,
+    Button
    } from "@chakra-ui/react"; 
    import { useAlertContext } from "./AlertContext"; 
-   import { useRef } from "react"; 
+   import { useRef } from "react";
+   import { MdClose } from "react-icons/md";
     
    /** 
    * This is a global component that uses context to display a global alert message. 
@@ -17,20 +20,40 @@ import {
     const isSuccess = type === "success" 
     
     return ( 
-      <AlertDialog 
-        isOpen={isOpen} 
-        leastDestructiveRef={cancelRef} 
-        onClose={onClose} 
-      > 
-        <AlertDialogOverlay> 
-          <AlertDialogContent py={4} backgroundColor={isSuccess ? '#22c55e' : '#ef4444'} color={'#ffffff'}>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold"> 
-              {isSuccess ? 'Success!' : 'Oops!'} 
-            </AlertDialogHeader> 
-            <AlertDialogBody>{message}</AlertDialogBody> 
-          </AlertDialogContent> 
-        </AlertDialogOverlay> 
-      </AlertDialog> 
+      <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+      mx={{ base: 4, md: 0 }}
+    >
+      <AlertDialogOverlay
+        fontSize={{ base: 'sm', md: 'md', lg: 'lg' }}
+      >
+        <AlertDialogContent
+          py={1}
+          mx={{ base: 4, md: 0 }}
+          backgroundColor={isSuccess ? '#22c55e' : '#ef4444'}
+          color={'#ffffff'}
+        >
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            {isSuccess ? 'Success!' : 'Oops!'}
+            <Button  pos="absolute" top="2" right="2" onClick={onClose} colorScheme="none">
+                <MdClose fontSize="1.5rem" />
+            </Button>
+          </AlertDialogHeader>
+          <AlertDialogBody fontSize="1rem">{message}</AlertDialogBody>
+          <AlertDialogFooter
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Button onClick={onClose} p={4} textAlign="center" w="100%" h={4} colorScheme="whiteAlpha">
+              OK
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
     ); 
    } 
     
